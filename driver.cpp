53748beb0db8066747b920ebe9747c746d8999b6
JoeKuzko <90813383+JoeKuzko@users.noreply.github.com>
2021-10-21 15:32:01 -0500

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -4,13 +4,16 @@ using namespace std;
 
 int main(){
 	buffer mybuffer;
-	string FileName = "zipcode.csv";
-	//cout << "This program will take data from a CSV file and return the north-most, south-most, east-most,  \nand west-most zipcode for each state\n\n";
-	//cout << "Enter a .csv file: ";
-	//cin >> FileName;
+	string FileName;
+	cout << "This program will take data from a CSV file and return the north-most, south-most, east-most,  \nand west-most zipcode for each state\n\n";
+	cout << "Enter a .csv file: ";
+	cin >> FileName;
 	mybuffer.col_names(FileName);
 	mybuffer.convert();
 	mybuffer.Read(FileName);
+	mybuffer.LIFileHeader();
+	mybuffer.LIconvert();
+	mybuffer.LIwrite();
 	mybuffer.Write();
 
 }
